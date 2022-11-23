d19e467cc9b657958e8d883cef61e6203ee75bbe
Artturi Vehviläinen <artturi.vehvilainen@helsinki.fi>
2022-11-23 15:14:23 +0200

    Jäsennelty uudestaan

--- othello/src/board.py
+++ othello/src/board.py
@@ -1,8 +1,4 @@
 class Board:
-    """Laudan tila tallennetaan listaan, jossa 0 on tyhjä ruutu,
-    1 on musta nappula ja 2 on valkoinen nappula.
-    Pelaajan vuoro tallennetaan boolean muuttujaan, jossa
-    True on mustan vuoro ja False valkoisen."""
 
     def __init__(self):
         self.board_state = [[0, 0, 0, 0, 0, 0, 0, 0],
@@ -25,13 +21,9 @@ class Board:
             return True
         return False
 
-    """Funktio ottaa vastaan ruudun ja tarkistaa onko ruutu laillinen siirto.
-    Jos siirto on laillinen, muuttaa se laudan tilaa ja palauttaa True.
-    Jos siirto on laiton, palauttaa funktio False eikä muuta lautaa."""
-
-    def make_move(self, y, x):
-        if (y, x) in self.legal_list:
-            self.alter_board(y, x)
+    def make_move(self, y_square, x_square):
+        if (y_square, x_square) in self.legal_list:
+            self.alter_board(y_square, x_square)
             return True
         return False
 
@@ -44,29 +36,26 @@ class Board:
             i += 1
         print()
 
-    def alter_board(self, y, x):
-        for move in self.legal_list[(y, x)]:
-            self.fill(y, x, move)
+    def alter_board(self, y_square, x_square):
+        for move in self.legal_list[(y_square, x_square)]:
+            self.fill(y_square, x_square, move)
 
-    def fill(self, y, x, move):
+    def fill(self, y_square, x_square, move):
         if self.player:
             pnum = 1
         else:
             pnum = 2
-        dist = max(abs(y-move[0]), abs(x-move[1]))
-        if y == move[0]:
+        dist = max(abs(y_square-move[0]), abs(x_square-move[1]))
+        if y_square == move[0]:
             y_step = 0
         else:
-            y_step = (move[0]-y)//abs((move[0]-y))
-        if x == move[1]:
+            y_step = (move[0]-y_square)//abs((move[0]-y_square))
+        if x_square == move[1]:
             x_step = 0
         else:
-            x_step = (move[1]-x)//abs((move[1]-x))
+            x_step = (move[1]-x_square)//abs((move[1]-x_square))
         for i in range(dist+1):
-            self.board_state[y+(i*y_step)][x+(i*x_step)] = pnum
-
-    """Funktio käy läpi laudan ruudut. Jokaisen löytämämsä tyhjän ruudun kohdalla
-    funktio antaa ruudun legal_check() funktiolle tarkistettavaksi"""
+            self.board_state[y_square+(i*y_step)][x_square+(i*x_step)] = pnum
 
     def legal_moves(self):
         moves = {}
@@ -81,11 +70,7 @@ class Board:
         self.legal_list = moves
         return moves
 
-    """Funktio käy läpi legal_moves() funktion antamat ruudut ja tarkistaa
-    onko niistä alkavia laillisia siirtoja.
-    Muuttujat pnum ja onum viittaavat sanoihin player number ja opponent number."""
-
-    def legal_check(self, y, x):
+    def legal_check(self, y_square, x_square):
         moves = []
         if self.player:
             pnum = 1
@@ -94,8 +79,8 @@ class Board:
             pnum = 2
             onum = 1
 
-        temp_y = y - 1
-        temp_x = x
+        temp_y = y_square - 1
+        temp_x = x_square
         if temp_y > 0 and self.board_state[temp_y][temp_x] == onum:
             while temp_y > 0:
                 temp_y -= 1
@@ -104,7 +89,7 @@ class Board:
                     break
                 if self.board_state[temp_y][temp_x] == 0:
                     break
-        temp_y = y + 1
+        temp_y = y_square + 1
         if temp_y < 7 and self.board_state[temp_y][temp_x] == onum:
             while temp_y < 7:
                 temp_y += 1
@@ -113,8 +98,8 @@ class Board:
                     break
                 if self.board_state[temp_y][temp_x] == 0:
                     break
-        temp_y = y
-        temp_x = x - 1
+        temp_y = y_square
+        temp_x = x_square - 1
         if temp_x > 0 and self.board_state[temp_y][temp_x] == onum:
             while temp_x > 0:
                 temp_x -= 1
@@ -123,7 +108,7 @@ class Board:
                     break
                 if self.board_state[temp_y][temp_x] == 0:
                     break
-        temp_x = x + 1
+        temp_x = x_square + 1
         if temp_x < 7 and self.board_state[temp_y][temp_x] == onum:
             while temp_x < 7:
                 temp_x += 1
@@ -132,8 +117,8 @@ class Board:
                     break
                 if self.board_state[temp_y][temp_x] == 0:
                     break
-        temp_y = y - 1
-        temp_x = x - 1
+        temp_y = y_square - 1
+        temp_x = x_square - 1
         if temp_y > 0 and temp_x > 0 and self.board_state[temp_y][temp_x] == onum:
             while temp_y > 0 and temp_x > 0:
                 temp_y -= 1
@@ -143,8 +128,8 @@ class Board:
                     break
                 if self.board_state[temp_y][temp_x] == 0:
                     break
-        temp_y = y + 1
-        temp_x = x + 1
+        temp_y = y_square + 1
+        temp_x = x_square + 1
         if temp_y < 7 and temp_x < 7 and self.board_state[temp_y][temp_x] == onum:
             while temp_y < 7 and temp_x < 7:
                 temp_y += 1
@@ -154,8 +139,8 @@ class Board:
                     break
                 if self.board_state[temp_y][temp_x] == 0:
                     break
-        temp_y = y - 1
-        temp_x = x + 1
+        temp_y = y_square - 1
+        temp_x = x_square + 1
         if temp_y > 0 and temp_x < 7 and self.board_state[temp_y][temp_x] == onum:
             while temp_y > 0 and temp_x < 7:
                 temp_y -= 1
@@ -165,8 +150,8 @@ class Board:
                     break
                 if self.board_state[temp_y][temp_x] == 0:
                     break
-        temp_y = y + 1
-        temp_x = x - 1
+        temp_y = y_square + 1
+        temp_x = x_square - 1
         if temp_y < 7 and temp_x > 0 and self.board_state[temp_y][temp_x] == onum:
             while temp_y < 7 and temp_x > 0:
                 temp_y += 1
